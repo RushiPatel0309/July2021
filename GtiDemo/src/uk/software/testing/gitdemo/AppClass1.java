bba19e65185515919d05e2e3cd165d9e5f699122
RushiPatel0309 <Brinshi2011>
2021-07-01 22:55:43 +0100

    Jira - TEST-123 Adding two new classes

---
+++ GtiDemo/src/uk/software/testing/gitdemo/AppClass1.java
@@ -0,0 +1,8 @@
+package uk.software.testing.gitdemo;
+
+public class AppClass1 {
+    public void doSomething(){
+        System.out.println("Doing Something");
+    }
+}
+
---
+++ GtiDemo/src/uk/software/testing/gitdemo/AppClass2.java
@@ -0,0 +1,7 @@
+package uk.software.testing.gitdemo;
+
+public class AppClass2 {
+    public void doSomething(){
+        System.out.println("Doing Something from App Class");
+    }
+}
